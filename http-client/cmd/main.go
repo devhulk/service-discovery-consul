50100b330feaf3844b51b202bd23a3646c2ed7a2
Gerald Yerden II <devhulk@Geralds-MBP.carolina.rr.com>
2020-07-01 15:54:10 -0400

    scaffolded project with example idl

---
+++ http-client/cmd/main.go
@@ -0,0 +1,65 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// Package main implements a client for Greeter service.
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	pb "local/articles/service-discovery-consul/http-client/internal/svca"
+
+	"google.golang.org/grpc"
+)
+
+const ()
+
+func connectSvc(address string, defaultMessage string) {
+
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	name := defaultMessage
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Sent: %s", r.GetMessage())
+}
+
+func main() {
+
+	address := "localhost:5000"
+	defaultMessage := "hello from svcA"
+
+	connectSvc(address, defaultMessage)
+}
---
+++ svc-a/cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "local/articles/service-discovery-consul/svc-a/internal/svca"
+
+	"google.golang.org/grpc/reflection"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":5000"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: in.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+
+	reflection.Register(s)
+
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
